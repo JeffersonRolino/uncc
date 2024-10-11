d47aefdcb1e5ef59c0dedd410ad596797c585364
jeffersonrolino <jefferson.rolino@gmail.com>
2024-10-11 16:23:06 -0300

    Chapter 04 - Lesson 026: Reading the Content of the Command File

--- 04-file-system/file-commander-app/app.js
+++ 04-file-system/file-commander-app/app.js
@@ -1,12 +1,30 @@
 const fs = require("fs/promises");
 
 (async () => {
+    const commandFileHandler = await fs.open("./command.txt", "r");
+
     const watcher = fs.watch("./command.txt");
 
     for await (const event of watcher) {
         if (event.eventType === "change") {
             //the file was changed...
             console.log("The file was changed...");
+
+            //Get the size of our file
+            const size = (await commandFileHandler.stat()).size;
+            const buffer = Buffer.alloc(size);
+
+            const offset = 0;
+            const length = buffer.byteLength;
+            const position = 0;
+
+            const content = await commandFileHandler.read(
+                buffer,
+                offset,
+                length,
+                position
+            );
+            console.log(content);
         }
     }
 })();
