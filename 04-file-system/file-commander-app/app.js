b8315493bcc287ecd4b32a2c2c79abfdda8307c7
jeffersonrolino <jefferson.rolino@gmail.com>
2024-10-11 16:31:45 -0300

    Chapter 04 - Lesson 027: Cleaning Up the Code using EventEmitter

--- 04-file-system/file-commander-app/app.js
+++ 04-file-system/file-commander-app/app.js
@@ -3,28 +3,38 @@ const fs = require("fs/promises");
 (async () => {
     const commandFileHandler = await fs.open("./command.txt", "r");
 
+    commandFileHandler.on("change", async () => {
+        //Get the size of our file
+        const size = (await commandFileHandler.stat()).size;
+
+        //Allocate our buffer with the size of the file
+        const buffer = Buffer.alloc(size);
+
+        //The location at which we want to start filling our buffer
+        const offset = 0;
+
+        //How many bytes we want to read
+        const length = buffer.byteLength;
+
+        //The position that we want to start reading from;
+        const position = 0;
+
+        //We always want to read the whole content
+        const content = await commandFileHandler.read(
+            buffer,
+            offset,
+            length,
+            position
+        );
+        console.log(content);
+    });
+
+    //Create Watcher...
     const watcher = fs.watch("./command.txt");
 
     for await (const event of watcher) {
         if (event.eventType === "change") {
-            //the file was changed...
-            console.log("The file was changed...");
-
-            //Get the size of our file
-            const size = (await commandFileHandler.stat()).size;
-            const buffer = Buffer.alloc(size);
-
-            const offset = 0;
-            const length = buffer.byteLength;
-            const position = 0;
-
-            const content = await commandFileHandler.read(
-                buffer,
-                offset,
-                length,
-                position
-            );
-            console.log(content);
+            commandFileHandler.emit("change");
         }
     }
 })();
